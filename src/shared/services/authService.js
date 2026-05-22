3edcb3f5d7b0ac97dedb05f67cebdea6fe38e1c7
Andrey <2119163@gmail.com>
2026-05-22 10:05:42 +0300

    Update authService.js

--- src/shared/services/authService.js
+++ src/shared/services/authService.js
@@ -1,34 +1,13 @@
-import { computed } from 'vue'
 import { useAuth } from '@/app/plugins/JwtAuthViaAxios'
 
 export const useAuthService = () => {
-  const {
-    login: libLogin,
-    logout: libLogout,
-    isAuthenticated: libIsAuthenticated,
-    user: libUser,
-  } = useAuth()
-
-  const login = async credentials => {
-    return await libLogin(credentials)
-  }
-
-  const logout = () => {
-    return libLogout()
-  }
-
-  const isAuthenticated = computed(() => {
-    return libIsAuthenticated.value
-  })
-
-  const user = computed(() => {
-    return libUser.value
-  })
+  const auth = useAuth()
 
   return {
-    login,
-    logout,
-    isAuthenticated,
-    user,
+    login: auth.login,
+    logout: auth.logout,
+    user: auth.user,
+    isReady: auth.isReady,
+    isAuthenticated: auth.isAuthenticated,
   }
 }
