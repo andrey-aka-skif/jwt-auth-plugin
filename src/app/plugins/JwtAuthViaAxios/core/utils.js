4fec1e31c4be6178e39c6481d028e07bddfdf86c
Andrey <2119163@gmail.com>
2026-06-04 16:34:50 +0300

    Update utils.js

--- src/app/plugins/JwtAuthViaAxios/core/utils.js
+++ src/app/plugins/JwtAuthViaAxios/core/utils.js
@@ -20,23 +20,3 @@ export const mergeConfigs = (target, source) => {
 
   return output
 }
-
-// удалить
-const decodeJwt = token => {
-  try {
-    const base64Url = token.split('.')[1]
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
-    return JSON.parse(window.atob(base64))
-  } catch {
-    return null
-  }
-}
-
-// удалить
-export const getTokenRemainingLifetimeMs = token => {
-  const decoded = decodeJwt(token)
-  if (!decoded?.exp) {
-    return null
-  }
-  return decoded.exp * 1000 - Date.now()
-}
